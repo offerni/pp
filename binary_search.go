7df4c5f93ea250cf55fdd1c62bde5d839d1441bd
offerni <drofferni@gmail.com>
2024-06-26 21:41:17 -0700

    reorganizing implementation details and preparing generic wait group and resultsChannel

--- binary_search.go
+++ binary_search.go
@@ -1,5 +1,40 @@
 package pp
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func BinaryVsSerialSearch(wg *sync.WaitGroup, resultsChannel chan<- string) {
+	start := time.Now()
+	ints := CreateLargeSliceMake(100000000)
+	elapsed := time.Since(start)
+	fmt.Printf("Created after %v milliseconds\n", elapsed.Milliseconds())
+	fmt.Println("Creating test data...")
+
+	const searchTerm = 99999999
+
+	fmt.Println("Searching...")
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		start := time.Now()
+		res := IntBinarySearch(searchTerm, ints)
+		elapsed := time.Since(start)
+		resultsChannel <- fmt.Sprintf("Result: %v. Binary search Took %v microseconds\n", res, elapsed.Microseconds())
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		start2 := time.Now()
+		res2 := IntSerialSearch(searchTerm, ints)
+		elapsed2 := time.Since(start2)
+		resultsChannel <- fmt.Sprintf("Result: %v. Normal search Took %v microseconds\n", res2, elapsed2.Microseconds())
+	}()
+}
+
 func IntBinarySearch(term int, data []int) bool {
 	start := 0
 	end := len(data) - 1
